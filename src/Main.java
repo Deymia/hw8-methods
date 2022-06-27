70f622a8f4633ca4c28bc41d10d97b477f6c857f
Anastasia <shvayka92@mail.ru>
2022-06-27 22:56:13 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.time.LocalDate;
+
 public class Main {
+//первое задание
+    public static boolean checkYear (int a){
+
+        boolean isLeap = false;
+        if ( a % 4==0 && a % 100!=0 || a % 400 ==0){
+            isLeap = true;
+        } else {
+            isLeap = false;
+        }
+        return isLeap;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//все еще первое задание
+        int year = 2005;
+        checkYear(year);
+        if (checkYear(year)==true) {
+            System.out.println("Год " + year + "високосный");
+
+        } if (!checkYear(year)) {
+            System.out.println("Год " + year + " не високосный");
+
+        }
+//второе задание
+
+    }
+    public static boolean checkOSAndVersion (int a, int b ){
+        int OS = 0;
+        int version = 2015;
+        int currentYear = LocalDate.now().getYear();
+        if (OS==0 && version < currentYear){
+
+        }
+
     }
 }
